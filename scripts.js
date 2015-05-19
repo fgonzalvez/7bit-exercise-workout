487369198df17a19748121844d33219035433230
Paco Gonzálvez <mad8bits@gmail.com>
2015-05-19 20:54:02 +0200

    Something new

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+var exercises = ['Jumping jacks', 'Wall sit', 'Push-up', 'Abdominal crunch', 
+				'Step-up onto chair', 'Squat', 'Triceps dip on chair', 'Plank', 
+				'High knees running in place', 'Lunge', 'Push-up and rotation', 'Side plank'];
+var globalCounter = 1;
+var powerUpAudio = new Audio("sounds/SFX_Powerup_01.wav");
+
 function id(el) {
 	return document.getElementById(el);
 }
@@ -5,33 +11,43 @@ function id(el) {
 function init() {
 	id('begin-button').style.display = 'none';
 	id('exercise').style.display = 'block';
+	id('bar').style.display = 'block';
 	exerciseCountDown();
 }
 
+function progressBar() {
+	id('progress').style.width = (globalCounter * 8.4) + "%";
+	powerUpAudio.play();
+}
+
 function exerciseCountDown() {
-	id('exercise').style.display = 'block';
-	id('rest').style.display = 'none';
-	var counter =  id('exercise').getElementsByClassName('counter')[0]
-	counter.innerHTML = 5;
-	var counterValue = counter.innerHTML;
-	console.log(counter);
-	var countDown = setInterval(function() {
-		counterValue--;
-		counter.innerHTML = counterValue;
+	if(globalCounter < 12) {
+		id('exercise').style.display = 'block';
+		id('rest').style.display = 'none';
+		var counter =  id('exercise').getElementsByClassName('counter')[0];
+		var title = id('exercise').getElementsByClassName('title')[0];
+		title.innerHTML = exercises[globalCounter - 1];
+		counter.innerHTML = 30;
+		var counterValue = counter.innerHTML;
 		console.log(counter);
-		if(counterValue==0) {
-			alert('seacabo');
-			clearInterval(countDown);
-			restCountDown();
-		}
-	}, 1000);
+		var countDown = setInterval(function() {
+			counterValue--;
+			counter.innerHTML = counterValue;
+			console.log(counter);
+			if(counterValue==0) {
+				clearInterval(countDown);
+				progressBar();
+				restCountDown();
+			}
+		}, 1000);		
+	}
 }
 
 function restCountDown() {
 	id('exercise').style.display = 'none';
 	id('rest').style.display = 'block';
 	var counter =  id('rest').getElementsByClassName('counter')[0]
-	counter.innerHTML = 5;
+	counter.innerHTML = 10;
 	var counterValue = counter.innerHTML;
 	console.log(counter);
 	var countDown = setInterval(function() {
@@ -39,8 +55,8 @@ function restCountDown() {
 		counter.innerHTML = counterValue;
 		console.log(counter);
 		if(counterValue==0) {
-			alert('seacabo');
 			clearInterval(countDown);
+			globalCounter++;
 			exerciseCountDown();
 		}
 	}, 1000);
